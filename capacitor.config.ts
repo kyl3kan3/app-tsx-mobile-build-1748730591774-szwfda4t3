840bb8e877cc1b33f166e1601956804b080c9bd8
kyl3kan3 <kyl3kan3@gmail.com>
2025-05-31 17:29:56 -0500

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aiappbuilder.apptsxmobilebuild1748730591774szwfda4t3',
+  appName: 'App.tsx',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
